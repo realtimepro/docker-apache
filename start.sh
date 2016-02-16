48cd1503a477f8a527d16bcb4f02cee9e53ad9a6
Duc Anh Babim <ducanh.babim@yahoo.com>
2016-02-16 18:26:19 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 service ssh start
+
+export TERM=xterm
+
+if [ -z "`etc/apache2/sites-available`" ] 
+then
+	rsync -a /etc-start/apache2/sites-available/* /etc/apache2/sites-available
+fi
+
 # Start apache
 /usr/sbin/apache2 -D FOREGROUND
 
